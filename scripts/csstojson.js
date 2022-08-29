938b83b8d458742b540304e39df57f1b9a136968
James McGrath <james@jamesmcgrath.net>
2022-08-29 16:45:34 +0900

    add filename json

--- scripts/csstojson.js
+++ scripts/csstojson.js
@@ -3,23 +3,34 @@ const postcssJs = require('postcss-js')
 const fs = require('fs')
 
 const folderPath = './dist/css'
+const targetPath = './dist/json'
 const files = fs.readdirSync(folderPath)
 
-files.forEach((file) => {
-  if (file === 'normalize.css') {
-    return
-  }
+if (files) {
+  const filenames = { ...files }
+  fs.writeFile(
+    `${targetPath}/filenames.json`,
+    JSON.stringify(filenames),
+    (err) => {
+      console.log(err)
+    }
+  )
 
-  try {
-    const css = fs.readFileSync(`${folderPath}/${file}`, 'utf8')
-    const root = postcss.parse(css)
-    const postCSSObj = postcssJs.objectify(root)
-    const filename = file.split('.')[0]
-    fs.writeFileSync(
-      `./dist/json/${filename}.json`,
-      JSON.stringify(postCSSObj, null, 2)
-    )
-  } catch (err) {
-    console.error(err)
-  }
-})
+  files.forEach((file) => {
+    if (file === 'normalize.css') {
+      return
+    }
+    try {
+      const css = fs.readFileSync(`${folderPath}/${file}`, 'utf8')
+      const root = postcss.parse(css)
+      const postCSSObj = postcssJs.objectify(root)
+      const filename = file.split('.')[0]
+      fs.writeFileSync(
+        `${targetPath}/${filename}.json`,
+        JSON.stringify(postCSSObj, null, 2)
+      )
+    } catch (err) {
+      console.error(err)
+    }
+  })
+}
