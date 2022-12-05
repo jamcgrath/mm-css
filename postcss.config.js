e031fefd052f9ec113d6494ba6c45e9daa5372a9
James <jamcgrath@users.noreply.github.com>
2022-12-05 16:32:02 +0900

    refactor to work with postcss8

--- postcss.config.js
+++ postcss.config.js
@@ -1,26 +1,26 @@
 const breakpoints = [
-  { prefix: 'sm-', mediaQuery: '(min-width: 480px)' },
-  { prefix: 'md-', mediaQuery: '(min-width: 768px)' },
-  { prefix: 'lg-', mediaQuery: '(min-width: 1024px)' },
-  { prefix: 'xl-', mediaQuery: '(min-width: 1440px)' },
-  { prefix: 'xxl-', mediaQuery: '(min-width: 1920px)' },
-]
+	{ prefix: "sm-", mediaQuery: "(min-width: 480px)" },
+	{ prefix: "md-", mediaQuery: "(min-width: 768px)" },
+	{ prefix: "lg-", mediaQuery: "(min-width: 1024px)" },
+	{ prefix: "xl-", mediaQuery: "(min-width: 1440px)" },
+	{ prefix: "xxl-", mediaQuery: "(min-width: 1920px)" },
+];
 
 const responsifyOptions = {
-  breakpoints,
-}
+	breakpoints,
+};
 
 module.exports = {
-  plugins: [
-    require('postcss-import'),
-    require('./postcss/postcss-responsify/index.js')(responsifyOptions),
-    require('postcss-preset-env')({
-      stage: 2,
-      autoprefixer: { flexbox: false },
-      postcssCustomProperties: {
-        preserve: true,
-        importFrom: './src/css/media-queries-props.css',
-      },
-    }),
-  ],
-}
+	plugins: [
+		require("postcss-import"),
+		require("./postcss/postcss-responsify/index.js")(responsifyOptions),
+		require("postcss-preset-env")({
+			stage: 2,
+			autoprefixer: { flexbox: false },
+			postcssCustomProperties: {
+				preserve: true,
+				importFrom: "./src/css/media-queries-props.css",
+			},
+		}),
+	],
+};
