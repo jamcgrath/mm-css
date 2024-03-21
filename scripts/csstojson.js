dd1ebcda72a55e470ccd4b6062fdb00c959c9614
James <james@jamesmcgrath.net>
2024-03-22 08:32:55 +0900

    update scripts

--- scripts/csstojson.js
+++ scripts/csstojson.js
@@ -1,13 +1,32 @@
+const fs = require('fs');
+const path = require('path');
 const postcss = require('postcss')
 const postcssJs = require('postcss-js')
-const fs = require('fs')
+
+function readFilesRecursively(dir, fileList = []) {
+    const files = fs.readdirSync(dir);
+
+    files.forEach(file => {
+        const filePath = path.join(dir, file);
+        const fileStat = fs.statSync(filePath);
+  
+        if (fileStat.isDirectory()) {
+            readFilesRecursively(filePath, fileList);
+        } else {
+            fileList.push(filePath.split('css/')[1]);
+        }
+    });
+
+    return fileList;
+}
 
 const folderPath = './dist/css'
+const files = readFilesRecursively(folderPath);
 const targetPath = './dist/json'
-const files = fs.readdirSync(folderPath)
 
 if (files) {
   const filenames = { ...files }
+
   fs.writeFile(
     `${targetPath}/filenames.json`,
     JSON.stringify(filenames),
@@ -15,7 +34,6 @@ if (files) {
       console.log(err)
     }
   )
-
   files.forEach((file) => {
     if (file === 'normalize.css') {
       return
@@ -24,7 +42,7 @@ if (files) {
       const css = fs.readFileSync(`${folderPath}/${file}`, 'utf8')
       const root = postcss.parse(css)
       const postCSSObj = postcssJs.objectify(root)
-      const filename = file.split('.')[0]
+      const filename = file.split('/')[1].split('.')[0]
       fs.writeFileSync(
         `${targetPath}/${filename}.json`,
         JSON.stringify(postCSSObj, null, 2)
