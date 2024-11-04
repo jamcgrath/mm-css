92cb224bd780dc36be60eb22c89bfa9e3b04196f
James <james@jamesmcgrath.net>
2024-11-04 09:38:03 +0900

    remove @responsive and build

--- scripts/csstojson.js
+++ scripts/csstojson.js
@@ -1,54 +1,54 @@
-const fs = require('fs');
-const path = require('path');
-const postcss = require('postcss')
-const postcssJs = require('postcss-js')
+const fs = require("fs");
+const path = require("path");
+const postcss = require("postcss");
+const postcssJs = require("postcss-js");
 
 function readFilesRecursively(dir, fileList = []) {
-    const files = fs.readdirSync(dir);
+  const files = fs.readdirSync(dir);
 
-    files.forEach(file => {
-        const filePath = path.join(dir, file);
-        const fileStat = fs.statSync(filePath);
-  
-        if (fileStat.isDirectory()) {
-            readFilesRecursively(filePath, fileList);
-        } else {
-            fileList.push(filePath.split('css/')[1]);
-        }
-    });
+  files.forEach((file) => {
+    const filePath = path.join(dir, file);
+    const fileStat = fs.statSync(filePath);
 
-    return fileList;
+    if (fileStat.isDirectory()) {
+      readFilesRecursively(filePath, fileList);
+    } else {
+      fileList.push(filePath.split("css/")[1]);
+    }
+  });
+
+  return fileList;
 }
 
-const folderPath = './dist/css'
+const folderPath = "./dist/css";
 const files = readFilesRecursively(folderPath);
-const targetPath = './dist/json'
+const targetPath = "./dist/json";
 
 if (files) {
-  const filenames = { ...files }
+  const filenames = { ...files };
 
   fs.writeFile(
     `${targetPath}/filenames.json`,
     JSON.stringify(filenames),
     (err) => {
-      console.log(err)
+      console.log(err);
     }
-  )
+  );
   files.forEach((file) => {
-    if (file === 'normalize.css') {
-      return
+    if (file === "normalize.css") {
+      return;
     }
     try {
-      const css = fs.readFileSync(`${folderPath}/${file}`, 'utf8')
-      const root = postcss.parse(css)
-      const postCSSObj = postcssJs.objectify(root)
-      const filename = file.split('/')[1].split('.')[0]
+      const css = fs.readFileSync(`${folderPath}/${file}`, "utf8");
+      const root = postcss.parse(css);
+      const postCSSObj = postcssJs.objectify(root);
+      const filename = file.split("/")[1].split(".")[0];
       fs.writeFileSync(
         `${targetPath}/${filename}.json`,
         JSON.stringify(postCSSObj, null, 2)
-      )
+      );
     } catch (err) {
-      console.error(err)
+      console.error(err);
     }
-  })
+  });
 }
